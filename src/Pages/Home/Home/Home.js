e5bc8cff904371245fb266ccc11e2c40e0579229
mjaumi <mjaumi2864@gmail.com>
2022-05-22 02:57:04 +0600

    initial commit

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageTitle from '../../Shared/PageTitle/PageTitle';
+
+const Home = () => {
+    return (
+        <section>
+            <PageTitle title={'Home'} />
+            <h1>This is home</h1>
+        </section>
+    );
+};
+
+export default Home;
